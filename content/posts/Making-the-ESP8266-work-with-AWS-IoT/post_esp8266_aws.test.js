3a7a7e0d226302e4d2a6e6c123497953c5e2e326
raphberube <raphael.berube@telus.com>
2020-09-09 07:24:36 -0400

    Image snapshot unit tests

---
+++ content/posts/Making-the-ESP8266-work-with-AWS-IoT/post_esp8266_aws.test.js
@@ -0,0 +1,43 @@
+const puppeteer = require("puppeteer");
+
+describe("jest-image-snapshot usage with an image received from puppeteer", () => {
+  let browser;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+  });
+
+  it("Post ESP8266 AWS", async () => {
+    const page = await browser.newPage();
+    await page.setDefaultTimeout(0);
+    const defaultViewport = {
+      height: 1920,
+      width: 1280,
+    };
+
+    console.log("page.goto");
+    await page.goto(
+      "http://localhost:8000/how-to-deploy-vue-press-on-google-firebase-using-cloud-build",
+      { waitUntil: "networkidle2" }
+    );
+    //await page.waitForFunction("window.ready");
+    console.log("wait window ready");
+
+    // Resize the viewport to screenshot elements outside of the viewport
+    const bodyHandle = await page.$("body");
+    const boundingBox = await bodyHandle.boundingBox();
+    const newViewport = {
+      width: Math.max(defaultViewport.width, Math.ceil(boundingBox.width)),
+      height: Math.max(defaultViewport.height, Math.ceil(boundingBox.height)),
+    };
+    await page.setViewport(Object.assign({}, defaultViewport, newViewport));
+
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+});
