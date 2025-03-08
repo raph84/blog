65bfdcc07972af553656febbd84f3eca0ff0b3f4
raph84 <raph.berube@gmail.com>
2025-03-08 13:04:15 -0500

    chore: eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import eslint from '@eslint/js';
+import { globalIgnores } from "eslint/config";
+import tseslint from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  //tseslint.configs.recommended,
+  //tseslint.configs.strict,
+  //tseslint.configs.stylistic,
+  tseslint.configs.strictTypeChecked,    // Typed linting (slower)
+  tseslint.configs.stylisticTypeChecked, // Typed linting (slower)
+  {
+    // typed linting settings
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslintPluginAstro.configs['flat/recommended'],
+  eslintPluginAstro.configs['flat/jsx-a11y-recommended'],
+  globalIgnores(["content/"]), // ignore old content folder
+  globalIgnores([".astro/"]), 
+  {
+    // override/add rules settings here, such as:
+    // "astro/no-set-html-directive": "error"
+  },
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+        projectService: false,
+        project: true
+      }
+    }
+  }
+);
