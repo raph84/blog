a0328e41fd979be2f40e3f5a88476eedabf35538
raph84 <raph.berube@gmail.com>
2025-03-08 21:43:39 -0500

    chore: disable eslint strictTypeChecked

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,20 +6,20 @@ import eslintPluginAstro from 'eslint-plugin-astro';
 export default tseslint.config(
   eslint.configs.recommended,
   //tseslint.configs.recommended,
-  //tseslint.configs.strict,
-  //tseslint.configs.stylistic,
-  tseslint.configs.strictTypeChecked, // Typed linting (slower)
-  tseslint.configs.stylisticTypeChecked, // Typed linting (slower)
-  {
-    // typed linting settings
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  // tseslint.configs.strictTypeChecked, // Typed linting (slower)
+  // tseslint.configs.stylisticTypeChecked, // Typed linting (slower)
+  // {
+  //   // typed linting settings
+  //   languageOptions: {
+  //     parserOptions: {
+  //       projectService: true,
+  //       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  //       tsconfigRootDir: import.meta.dirname,
+  //     },
+  //   },
+  // },
   eslintPluginAstro.configs['flat/recommended'],
   eslintPluginAstro.configs['flat/jsx-a11y-recommended'],
   globalIgnores(['content/']), // ignore old content folder
@@ -29,15 +29,15 @@ export default tseslint.config(
     // override/add rules settings here, such as:
     // "astro/no-set-html-directive": "error"
   },
-  {
-    files: ['**/*.astro'],
-    languageOptions: {
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
-        projectService: false,
-        project: true,
-      },
-    },
-  },
+  // {
+  //   files: ['**/*.astro'],
+  //   languageOptions: {
+  //     parserOptions: {
+  //       parser: '@typescript-eslint/parser',
+  //       extraFileExtensions: ['.astro'],
+  //       projectService: false,
+  //       project: true,
+  //     },
+  //   },
+  // },
 );
