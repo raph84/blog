5a4aa219ea34a8bf1d02abfacfda282423c139b6
raph84 <raph.berube@gmail.com>
2025-03-20 08:51:02 -0400

    fix: firebase google auth

--- src/firebase/firebaseApp.ts
+++ src/firebase/firebaseApp.ts
@@ -1,7 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import {
+  connectAuthEmulator,
+  getAuth,
+  GoogleAuthProvider,
+} from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -30,3 +34,8 @@ if (import.meta.env.MODE === 'development') {
   const auth = getAuth(getFirebaseApp());
   connectAuthEmulator(auth, 'http://127.0.0.1:9099');
 }
+
+export const googleAuthProvider = new GoogleAuthProvider();
+//provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+export const auth = getAuth(getFirebaseApp());
