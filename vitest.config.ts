54ba10d308de06251ebd30b331961a7437497ff4
raph84 <raph.berube@gmail.com>
2025-03-29 09:57:15 -0400

    chore: unit test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,9 @@ import { getViteConfig } from 'astro/config';
 export default getViteConfig({
   test: {
     include: ['src/**/*.test.*'],
+    coverage: {
+      include: ['src/**/*.astro', 'src/**/*.ts', 'src/**/*.tsx'],
+      provider: 'v8',
+    },
   },
 });
