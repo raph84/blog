966d38308e68b0d42b71ab1567ad4c37846541e6
raph84 <raph.berube@gmail.com>
2025-04-06 12:28:42 -0400

    chore: vitest react unit test (wip)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,13 @@
 /// <reference types="vitest" />
 import { getViteConfig } from 'astro/config';
+import react from '@vitejs/plugin-react';
 
 export default getViteConfig({
+  plugins: [react()],
   test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./vitest.setup.ts'],
     include: ['src/**/*.test.*'],
     coverage: {
       include: ['src/**/*.astro', 'src/**/*.ts', 'src/**/*.tsx'],
