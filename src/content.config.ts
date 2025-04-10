31afaadab94ee0d56c68bc4c5c67cbab23031bcf
raph84 <raph.berube@gmail.com>
2025-04-10 19:09:46 -0400

    feat: technotes section

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,9 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+const technotes = defineCollection({
+  // Load Markdown and MDX files in the `src/content/technotes` directory.
+  loader: glob({ base: './src/content/technotes', pattern: '**/*.{md,mdx}' }),
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
@@ -15,4 +15,4 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+export const collections = { technotes };
